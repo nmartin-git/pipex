5491e9ef94013dcd2f49d25c4776a6bbef82ba12
nmartin-git <nmartin.contactpro@gmail.com>
2025-02-05 15:57:39 +0100

    Update ppx_utils.c

--- git_pipex_saved/srcs/ppx_utils.c
+++ git_pipex_saved/srcs/ppx_utils.c
@@ -23,35 +23,6 @@ void	ppx_exit(int err, const char *str, int *fd_pipe, int status)
 	}
 }
 
-char	**ppx_setcmd(char **cmd, char *f, int fd, int *fdp)
-{
-	int		size_tab;
-	char	**cmd_p;
-
-	size_tab = 0;
-	if (!cmd || !f)
-		return (free(f), ppx_close(fd, fdp, 1, cmd), NULL);
-	while (cmd[size_tab])
-		size_tab++;
-	cmd_p = malloc(sizeof(char *) * (size_tab + 2));
-	if (!cmd_p)
-		return (free(f), ppx_close(fd, fdp, 1, cmd), NULL);
-	size_tab = 0;
-	while (cmd[size_tab])
-	{
-		cmd_p[size_tab] = ft_strdup(cmd[size_tab]);
-		if (!cmd_p[size_tab])
-		{
-			ft_free_tab(cmd);
-			return (free(f), ppx_close(fd, fdp, 1, cmd_p), NULL);
-		}
-		size_tab++;
-	}
-	cmd_p[size_tab++] = f;
-	cmd_p[size_tab] = NULL;
-	return (ft_free_tab(cmd), cmd_p);
-}
-
 void	ppx_close(int fd, int *fd_pipe, int err, char **cmd)
 {
 	if (cmd)
